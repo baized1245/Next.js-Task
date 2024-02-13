5400a3baaeb2084d6f2d78f4351224ebec6549ae
unknown <you@example.com>
2024-02-13 07:18:12 +0600

    first commit

---
+++ next-task/src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen">
+      <h1 className="text-center text-3xl font-bold text-rose-800 my-auto">
+        Next Task
+      </h1>
+    </div>
+  )
+}
